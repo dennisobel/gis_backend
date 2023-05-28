5d27eb729f8a0f8ad0954eea7efc85d826387319
dennisobel <ogembodenis2016@gmail.com>
2023-05-28 16:04:42 +0300

    restore from backup

--- controllers/building.js
+++ controllers/building.js
@@ -65,6 +65,49 @@ import Building from "../models/Building.js";
 //   "prevPage": null,
 //   "nextPage": 2
 // }
+// Create a new building
+export const createBuilding = async (req, res) => {
+  try {
+    const buildingData = req.body;
+    const building = new Building(buildingData);
+    const newBuilding = await building.save();
+    res.status(201).json(newBuilding);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Read all buildings
+export const getAllBuildings = async (req, res) => {
+  try {
+    const buildings = await Building.find();
+    res.status(200).json(buildings);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Read all county buildings
+export const getAllCountyBuildings = async (req, res) => {
+  const county = req.params.county;
+  try {
+    const buildings = await Building.aggregate([
+      { $match: { county } },
+      {
+        $lookup: {
+          from: "singlebusinesspermits",
+          localField: "_id",
+          foreignField: "building",
+          as: "singleBusinessPermits",
+        },
+      },
+    ]);
+
+    res.status(200).json(buildings);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 export const getBuildings = async (req, res) => {
   const {
@@ -113,6 +156,39 @@ export const getBuildingById = async (req, res) => {
     res.status(200).json(building);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+// Update a building
+export const updateBuildingById = async (req, res) => {
+  try {
+    const { buildingId } = req.params;
+    const updatedData = req.body;
+    const updatedBuilding = await Building.findByIdAndUpdate(
+      buildingId,
+      updatedData,
+      { new: true }
+    );
+    if (!updatedBuilding) {
+      return res.status(404).json({ error: "Building not found" });
+    }
+    res.status(200).json(updatedBuilding);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Delete a building
+export const deleteBuildingById = async (req, res) => {
+  try {
+    const { buildingId } = req.params;
+    const deletedBuilding = await Building.findByIdAndDelete(buildingId);
+    if (!deletedBuilding) {
+      return res.status(404).json({ error: "Building not found" });
+    }
+    res.status(200).json({ message: "Building deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 };
 
